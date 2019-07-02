7e192c092719cb776a2875a293e15044362ba3e8
victorpena13 <victorpena@Victors-MacBook-Pro.local>
2019-07-02 14:04:22 -0500

    jquery selectors

---
+++ js/jQuery1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$('body').css('background-color', 'light-gray');
+$('.headers').css('color', 'orange');
+$('#last').css('color', 'purple')
+// alert($('#last').html());
+$('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+$('h1, li, p').css('background-color', 'yellow');
