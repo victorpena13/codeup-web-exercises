df2baee624f37a11db31a690927f75b79880b0b1
victorpena13 <victorpena@Victors-MacBook-Pro.local>
2019-07-02 15:07:00 -0500

    mouse events

--- js/jQuery1.js
+++ js/jQuery1.js
@@ -1,10 +1,27 @@
 'use strict';
 
-$('body').css('background-color', 'light-gray');
-$('.headers').css('color', 'orange');
-$('#last').css('color', 'purple')
-// alert($('#last').html());
-$('.codeup').css('border', '1px solid red');
+// $('body').css('background-color', 'light-gray');
+// $('.headers').css('color', 'orange');
+// $('#last').css('color', 'purple')
+// // alert($('#last').html());
+// $('.codeup').css('border', '1px solid red');
+//
+// $('li').css('font-size', '20px');
+// $('h1, li, p').css('background-color', 'yellow');
 
-$('li').css('font-size', '20px');
-$('h1, li, p').css('background-color', 'yellow');
+
+    $('h1').click(function(){
+        $(this).css('background-color', 'orange')
+    });
+
+    $('p').dblclick(function(){
+        $(this).css('font-size', '18px')
+    });
+    $('li').hover(
+        function(){
+        $(this).css('color', 'red')
+        } ,
+        function(){
+    $(this).css('color', 'black')
+    }
+    );
