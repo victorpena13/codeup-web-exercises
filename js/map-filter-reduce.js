9b917517555794ac247a23b604b8f72fb023ca9e
victorpena13 <you@example.com>
2019-07-17 15:07:14 -0500

    completed

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -47,14 +47,33 @@ const totalYearsExperience = users.reduce(function(accumulation, current){
     const currentYears = current.yearsOfExperience;
     return currentYears + accumulation;
 },0);
-
+console.log("average: " + totalYearsExperience/users.length)
 
 const longestEmail = users.reduce(function(previous, current){
     const currentEmail = current.email;
     if(currentEmail.length > previous.length){
         return currentEmail
     } else {
-        return previuos;
+        return previous;
     }
 },"");
 
+
+const instructors = users.reduce(function(previous, current, index, arr){
+    if(index === (arr.length - 1)){
+        return previous + ` ${current.name}.` ;
+
+    }
+    return previous + ` ${current.name},` ;
+},"You're instructors are:");
+
+
+// Use .reduce to get the unique list of languages from the list of users.
+
+const uniquelanguages = users.reduce(function(prev, current){
+   const langs = current.languages;
+   for(const lang of langs){
+       prev.add(lang);
+   }
+   return prev;
+},new Set);
