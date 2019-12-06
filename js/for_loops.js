7181649f8569b6f9a872b870a139bd36413dc6e3
victorpena13 <victor.pena1392@gmail.com>
2019-12-06 12:52:24 -0600

    updated weather map commented code

--- js/for_loops.js
+++ js/for_loops.js
@@ -27,12 +27,6 @@ for (var i = 0; i < 11; i++) {
 }
 
 
-// An ice cream seller can't go home until she sells all of her cones. Write a JS program that ' +
-// 'generates a random number between 50 and 100 representing the amount of cones to sell. ' +
-// 'Your code should generate numbers between 1 and 5, simulating the amount of cones being ' +
-// 'bought by her clients. Use a do-while loop to log to the console the amount of cones ' +
-// 'sold to each person. This is how you get the random numbers.
-
 
 
 
