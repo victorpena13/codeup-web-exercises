0bfeecbd6f7f71f9c94aeee8e65065c8ca6e932e
victorpena13 <victor.pena1392@gmail.com>
2019-12-05 18:31:48 -0600

    started a new random number for exercise 2

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,14 +1,20 @@
+// Create a function named showMultiplicationTable that accepts a number and console.logs
+// the multiplication table for that number (just multiply by the numbers 1 through 10)
+
 function showMultiplicationTable(number) {
     for (var i = 0; i < 11; i++) {
         console.log(i + "*" + number + "=" + (number * i));
     }
 }
-var randomNumber = Math.floor(Math.random() * 6);
+var randomNumber = Math.floor(Math.random() * 11);
 
 showMultiplicationTable(randomNumber);
 
+// Use a for loop and the code from the previous lessons to generate 10 random numbers
+// between 20 and 200 and output to the console whether each number is odd or even.
+//
 
-
+var randomNum = Math.floor(Math.random() * 101 - 18);
 
 
 
