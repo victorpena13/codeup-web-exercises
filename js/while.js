80d5fb3b6126adb8dbf2a90c9a6822804e9c29e4
victorpena13 <victor.pena1392@gmail.com>
2019-12-05 19:30:23 -0600

    completed first exercise for while.js

--- js/while.js
+++ js/while.js
@@ -1,4 +1,5 @@
-"use strict"
+"use strict";
+
 
 var i = 2;
 while (i <= 65536) {
@@ -6,26 +7,40 @@ while (i <= 65536) {
     i=i*2
 }
 
-// This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
 
 
-var allCones = Math.floor(Math.random() * 50) + 50;
-console.log("starting the day with" + allCones + ' cones.')
 
-do {
-    var cones= Math.floor(Math.random() * 5) + 1;
-    if (cones > allCones) {
-        console.log("i cannot sell you " + cones + ", I only have " + allCones + " cones.")
-    } else {
-        allCones -= cones;
-        console.log("I sold " + cones + " cones.");
-    }
-}
 
-while (allCones > 0);
-console.log("I'm all done ")
+
+
+// "use strict"
+//
+// var i = 2;
+// while (i <= 65536) {
+//     console.log(i);
+//     i=i*2
+// }
+//
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// // This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+//
+//
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// console.log("starting the day with" + allCones + ' cones.')
+//
+// do {
+//     var cones= Math.floor(Math.random() * 5) + 1;
+//     if (cones > allCones) {
+//         console.log("i cannot sell you " + cones + ", I only have " + allCones + " cones.")
+//     } else {
+//         allCones -= cones;
+//         console.log("I sold " + cones + " cones.");
+//     }
+// }
+//
+// while (allCones > 0);
+// console.log("I'm all done ")
 
 
