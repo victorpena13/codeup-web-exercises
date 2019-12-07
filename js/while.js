d6e8d0e5dd219523c4a7017564c53db8fed0ffee
victorpena13 <victor.pena1392@gmail.com>
2019-12-06 18:31:27 -0600

    completed the exercise again

--- js/while.js
+++ js/while.js
@@ -14,48 +14,20 @@ while (i <= 65536) {
 // 'bought by her clients. Use a do-while loop to log to the console the amount of cones ' +
 // 'sold to each person. This is how you get the random numbers.
 
-
-// This is how you get a random number between 50 and 100
+// // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-var randomNumber = Math.floor(Math.random() * 5) + 1
-
-
+// // This expression will generate a random number between 1 and 5
 
 do {
-    console.log("total cones: " + allCones + "sold: " + randomNumber + "left: " + (allCones - randomNumber));
-} while (randomNumber <= allCones);
+    var cones= Math.floor(Math.random() * 5) + 1;
+    if (cones > allCones) {
+        console.log("i cannot sell you " + cones + ", I only have " + allCones + " cones.")
+    } else {
+        allCones -= cones;
+        console.log("I sold " + cones + " cones.");
+    }
+}
 
-console.log("All Done");
-
-// "use strict"
-//
-// var i = 2;
-// while (i <= 65536) {
-//     console.log(i);
-//     i=i*2
-// }
-//
-// // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// // This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
-//
-//
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// console.log("starting the day with" + allCones + ' cones.')
-//
-// do {
-//     var cones= Math.floor(Math.random() * 5) + 1;
-//     if (cones > allCones) {
-//         console.log("i cannot sell you " + cones + ", I only have " + allCones + " cones.")
-//     } else {
-//         allCones -= cones;
-//         console.log("I sold " + cones + " cones.");
-//     }
-// }
-//
-// while (allCones > 0);
-// console.log("I'm all done ")
-
+while (allCones > 0);
+console.log("I'm all done ")
 
