d6bef82947859d7698dd9a5ef461b5f56b5e7d27
victor pena <victor.pena1392@Gmail.com>
2021-05-27 16:46:34 -0500

    created a function that outputs a list of odd numbers between 1 & 50 while determing if the user inputs an odd number between those peramiters

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,32 +1,43 @@
+// Prompt the user for an odd number between 1 and 50.
+// Use a loop and a break statement to continue prompting
+// the user if they enter invalid input.
+//     Use a loop and the continue statement to output all the
+// odd numbers between 1 and 50, except for the number the user
+// entered.
 
-do {
-    var userNum = prompt("enter an odd number between 1 & 50: ");
-    if (isNaN(userNum)) {
-        alert("That is not a number!");
-    } else if (userNum >= 50) {
-        alert("that number doesn't work");
-        var userNum = prompt("enter an odd number between 1 & 50: ");
+//an immediately invoked function that runs functions as soon as they are defined
+(function () {
+
+    var userInput = parseInt(prompt("Please enter an odd number between 1-50:"));
+    // outputs odd numbers between 1 and 100
+    function oddNumber() {
+        for (var i = 1; i <= 50; i=i+2) {
+            if (userInput % 2 === 0) {
+                break;
+            } else if (userInput === i) {
+                console.log("Yikes!, lets skip " + userInput);
+            }
+        }
     }
-} while (userNum % 2 === 0);
 
-
-for (var i = 0; i <= 50; i++) {
-    if (i === 27) {
-        console.log("Yikes skipping " + i + "!");
-    } else {
-        console.log("Here is an odd number" + i  + "!");
+    while(userInput > 50 || isNaN(userInput) === true || userInput % 2 === 0) {
+        alert("Hey! That's an invalid input. Try again");
+        userInput = parseInt(prompt("Please enter an odd number between 1-50:"));
     }
-}
+    console.log(oddNumber(userInput));
+
+    alert("The number to skip is: " + userInput);
+    console.log("The number to skip is: " + userInput);
+    console.log(oddNum(userInput));
+
+})();
 
 
-// var userNum = prompt("enter an odd number between 1 & 50: ");
-//
-// if (isNaN(userNum)) {
-//     alert
-// }
-//
-//
-//
+
+
+
+
+
 //
 //
 // //2 examples of how to make the above example a break and continue statement.
